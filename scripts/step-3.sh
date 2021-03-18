f93e55b6e6d5eb9a6953e247756574c41d377648
MsyuLuch <68269276+MsyuLuch@users.noreply.github.com>
2021-03-18 13:10:33 +0900

    Update step-3.sh

--- scripts/step-3.sh
+++ scripts/step-3.sh
@@ -46,7 +46,7 @@ ls -ld /var/lib/prometheus
 sayWait
 
 # создаем директорию для распаковки архивов
-mkdir $HOME_DIR/prometheus && cd $HOME_DIR/prometheus
+cd $HOME_DIR
 
 # скачиваем prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.25.0/prometheus-2.25.0.linux-amd64.tar.gz
@@ -59,7 +59,7 @@ sayWait
 ------------------------------------------------------------------
 #NODE-EXPORTER
 
-mkdir $HOME_DIR/node_exporter && cd $HOME_DIR/node_exporter
+cd $HOME_DIR
 
 # скачиваем node-exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
@@ -69,14 +69,14 @@ tar xvfz node_exporter*.t*gz
 
 sayWait
 
-# копируем файлы в папки, где будут хранится утилиты установленные пользователями 
+# копируем файлы в папки, где будут хранится утилиты установленные пользователями
 #и система будет их видеть
 cp $HOME_DIR/node_exporter-*/node_exporter /usr/local/bin
 
 # устанавливаем права на папку для пользователя node_exporter
 chown -v node_exporter /usr/local/bin/node_exporter
 
-# создаем unit для systemd, который позволит запуститить утилиту, просматривать статус 
+# создаем unit для systemd, который позволит запуститить утилиту, просматривать статус
 #выполнения и позволит добавить утилиту в автозагрузку
 
 #создаем файл сервиса node_exporter
@@ -93,7 +93,7 @@ sayWait
 -------------------------------------------------------------
 #PROMETHEUS
 
-cd $HOME_DIR/prometheus
+cd $HOME_DIR
 
 # копируем файлы все в /etc/prometheus
 cp -rvi prometheus-*/{console{_libraries,s},prometheus.yml} /etc/prometheus/
@@ -104,15 +104,12 @@ chown -Rv prometheus: /etc/prometheus/
 # настроить prometheus.yml
 cp $HOME_DIR/my-project/prometheus/* /etc/prometheus/prometheus.yml
 
-# копируем файлы в папки, где будут хранится утилиты установленные пользователями 
+# копируем файлы в папки, где будут хранится утилиты установленные пользователями
 #и система будет их видеть
 cp -vi $HOME_DIR/prometheus-*/prom{etheus,tool} /usr/local/bin
 
 # устанавливаем права на папку для пользователя node_exporter
 chown -v prometheus /usr/local/bin/prom{etheus,tool}
-
-# запускаем сам prometheus
-sudo -u prometheus /usr/local/bin/prometheus --config.file /etc/prometheus/prometheus.yml --storage.tsdb.path /var/lib/prometheus/ --web.console.templates=/etc/prometheus/consoles --web.console.libraries=/etc/prometheus/console_libraries
 
 # запускаем сервис и проверяем его состояние
 systemctl start prometheus.service && systemctl status prometheus.service
@@ -131,4 +128,4 @@ wget https://dl.grafana.com/oss/release/grafana-7.4.3-1.x86_64.rpm
 sudo yum install grafana-7.4.3-1.x86_64.rpm
 
 systemctl daemon-reload && systemctl start grafana-server && systemctl enable grafana-server && systemctl status grafana-server
-sayWait
+
