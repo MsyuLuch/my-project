a42e121eb6e271d4613467ea5564be421df81ae9
MsyuLuch <mashkinasu@gmail.com>
2021-03-18 00:08:17 +0000

    add cnf

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -11,10 +11,10 @@ mysql -e "FLUSH TABLES WITH READ LOCK; SET GLOBAL read_only = ON;"
 
 for database in `mysql -e'show databases;' | grep -v information_schema | grep -v Database`;
     do
-        for table in `mysql ${database} -e'show tables;'`;
+        for table in `mysql ${database} -e'show tables;' | grep -v Tables_*`;
         do
         /usr/bin/mysqldump -x --opt ${database} ${table} | /usr/bin/gzip -c > /var/backup/mysql/`date +%Y%m%d-%H%M`-$database-$table.sql.gz;
-        echo "[$(date +"%Y/%m/%d %H:%M:%S %Z")] INFO: dump $database:$table" >> /var/log/mysql/backup.log;
+        echo "[$(date +"%Y/%m/%d %H:%M:%S %Z")] INFO: dump $database:$table" >> /var/log/mysql/backup-`date +%Y%m%d`.log;
         done
 done
 
