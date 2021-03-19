e253ff994267059a8d27a77980f54c93bfee2b3f
MsyuLuch <68269276+MsyuLuch@users.noreply.github.com>
2021-03-19 10:19:14 +0900

    Update step-2.sh

--- scripts/step-2.sh
+++ scripts/step-2.sh
@@ -13,6 +13,7 @@ BACKUP_DIR="/var/backup/mysql"
 IP_MASTER="185.177.92.60"
 BIN_LOG="binlog.000002"
 POS_LOG="915"
+password="oTUSlave#2020"
 
 # Only root can execute this script
 USER_ID="$(id -u)"
@@ -22,39 +23,27 @@ if [[ ${USER_ID} -ne 0 ]]; then
 fi
 
 ############## MYSQL ###########################
-echo "установливаем mysql"
 yum install mysql-server
-
 systemctl start mysqld && systemctl status mysqld && systemctl enable mysqld
-
 mysql_secure_installation
-
 cp  $HOME_DIR/my-project/mysql/repl/* /etc/my.cnf.d/
 cp  $HOME_DIR/my-project/mysql/.my.cnf /root/
-
 service mysqld restart && systemctl status mysqld
 sayWait
-
+# загружаем дамп с master, установлена опция игнорировать дубликаты
 mysql --force < $HOME_DIR/my-project/dump-data.sql
 mysql -e "show databases;"
 sayWait
-
-mysql -e "CHANGE MASTER TO MASTER_HOST='$IP_MASTER', MASTER_USER='repl', MASTER_PASSWORD='oTUSlave#2020', MASTER_LOG_FILE='$BIN_LOG', MASTER_LOG_POS=$POS_LOG, GET_MASTER_PUBLIC_KEY = 1"
+# переводим сервер в режим slave
+mysql -e "CHANGE MASTER TO MASTER_HOST='$IP_MASTER', MASTER_USER='repl', MASTER_PASSWORD='$password', MASTER_LOG_FILE='$BIN_LOG', MASTER_LOG_POS=$POS_LOG, GET_MASTER_PUBLIC_KEY = 1"
 mysql -e "START SLAVE;"
 mysql -e "show slave status\G"
-
 sayWait
 echo "копируем скрипт backup.sh (/opt/scripts/)"
-
 mkdir -p /opt/scripts
 cp  $HOME_DIR/my-project/mysql/backup.sh /opt/scripts
 chmod 700 /opt/scripts/backup.sh
-
 mkdir -p $BACKUP_DIR
-
 echo "выполняем backup всех БД (потаблично)"
 /opt/scripts/backup.sh
-
-ls -l $BACKUP_DIR
-
 sayWait
