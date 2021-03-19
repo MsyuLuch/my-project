25562f525b60dd8531e2dc27f398b4929fb51ff2
MsyuLuch <68269276+MsyuLuch@users.noreply.github.com>
2021-03-19 10:16:22 +0900

    Update step-1.sh

--- scripts/step-1.sh
+++ scripts/step-1.sh
@@ -10,6 +10,7 @@ sayWait()
 
 HOME_DIR="/root"
 IP_REPL="185.177.93.227"
+password="oTUSlave#2020"
 
 # Only root can execute this script
 USER_ID="$(id -u)"
@@ -18,46 +19,36 @@ if [[ ${USER_ID} -ne 0 ]]; then
     exit 1
 fi
 
-############## APACHE & PHP ###########################
-echo "устанавливаем apache, php"
+############## APACHE & PHP ####################
 yum install httpd && dnf install php php-cli php-mysqlnd php-json php-gd php-ldap php-odbc php-pdo php-opcache php-pear php-xml php-xmlrpc php-mbstring php-snmp php-soap php-zip
 sayWait
 ############## NGINX ###########################
-echo "устанавливаем nginx"
 yum install nginx
 sayWait
-############## WORDPRESS ###########################
+############## WORDPRESS #######################
 cp -R $HOME_DIR/my-project/nginx/* /etc/nginx && cp -R $HOME_DIR/my-project/httpd/* /etc/httpd
-
 cd $HOME_DIR && wget https://ru.wordpress.org/latest-ru_RU.tar.gz && tar xzvf latest-ru_RU.tar.gz
-
 mkdir /var/www/site.ru && cp -R $HOME_DIR/wordpress/* /var/www/site.ru && chown -R apache. /var/www/site.ru
 sayWait
 ############## MYSQL ###########################
-echo "установливаем mysql"
 yum install mysql-server
-
 systemctl start mysqld && systemctl status mysqld && systemctl enable mysqld
-
 mysql_secure_installation
-
 cp  $HOME_DIR/my-project/mysql/my.cnf.d/* /etc/my.cnf.d/
 cp  $HOME_DIR/my-project/mysql/.my.cnf /root/
-
 service mysqld restart && systemctl status mysqld
-sayWait
-
-mysql -e "create user repl@"$IP_REPL" IDENTIFIED WITH caching_sha2_password BY 'oTUSlave#2020'"
+## создаем пользователя для репликации
+mysql -e "create user repl@"$IP_REPL" IDENTIFIED WITH caching_sha2_password BY '$password'"
 mysql -e "GRANT REPLICATION SLAVE ON *.* TO repl@"$IP_REPL
 mysql -e "SELECT User, Host FROM mysql.user"
-mysql -e "SHOW MASTER STATUS"
 sayWait
-
+# создаем базу данных для cms
 mysql -e "create database wordpress"
 mysql -e "show databases"
 sayWait
-
+# делаем полный дамп
 cd $HOME_DIR/my-project
 mysqldump --all-databases --no-create-info > dump-data.sql
+# выводим текущую информацию с master
 mysql -e "SHOW MASTER STATUS"
 sayWait
